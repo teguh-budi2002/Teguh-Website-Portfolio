43efdc5f4538baf24e1a5011b8359c3e0dac1569
teguh-budi2002 <budilaksono1102@gmail.com>
2023-06-08 11:34:20 +0700

    [UPDATE] Fix Some Bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./index.html", "./src/**/*.{html,js,vue}"],
+  content: ["./*.html", "./src/**/*.{html,js,vue}"],
   theme: {
     extend: {},
   },
